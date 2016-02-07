a5686a5185e36f35307c5860c60b7a18810797c6
gregmalcolm <greg@gregmalcolm.com>
2016-02-06 22:53:24 -0500

    Using bootstrap alerts for the flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def alert_type_from_flash(msg_type)
+    case msg_type
+      when "error"   then "alert-danger"
+      when "alert"   then "alert-warning"
+      when "success" then "alert-success"
+      else                "alert-info"
+    end
+  end
 end
